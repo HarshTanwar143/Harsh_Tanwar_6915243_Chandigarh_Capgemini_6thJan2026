c68192c07b82f973651a615d13cea92b2eb6072f
Harsh Tanwar <harshtanwar14122003@gmail.com>
2026-03-10 15:13:00 +0530

    commiting 10th March

---
+++ Week_10_9th_Mar_to_14th_Mar_2026/Day_2_10th_Mar_2026/EfCoreCodeBooks/Models/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreCodeBooks.Models
+{
+    [Table("tblBooks")]
+
+    public class BookModel
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        public string BookName { get; set; }
+        public int BookPrice { get; set; }
+        public int BookQuantity { get; set; }
+    }
+}
